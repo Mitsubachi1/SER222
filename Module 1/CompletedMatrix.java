65081b913b7e1a86afe5265fe3e58e7a2683603e
Mitsubachi1 <60766345+Mitsubachi1@users.noreply.github.com>
2023-10-14 10:12:14 -0700

    wa

--- Module 1/CompletedMatrix.java
+++ Module 1/CompletedMatrix.java
@@ -1,9 +1,9 @@
 package edu.ser222.m01_02;
 
 /**
- * An implementation of the Matrix ADT. Provides four basic operations over an immutable type.
+ * An implementation of the Matrix ADT. Provides four basic operations over an
+ * immutable type.
  * 
- * Last updated 7/31/2021.
  * 
  * @author Angel Chiquito, Ruben Acuna
  * @version 10/13/23
@@ -11,63 +11,98 @@ package edu.ser222.m01_02;
 
 public class CompletedMatrix implements Matrix {
 
-    private int[][] matrix; //for constructor, using int
-    public CompletedMatrix(int[][] matrix) { //constructor
+    private int[][] matrix; // for constructor, using int
+
+    public CompletedMatrix(int[][] matrix) { // constructor
+        if (matrix = NULL){
+            throw new IllegalArgumentException(); //edgecase null matrix
+        }
         this.matrix = matrix;
     }
 
     @Override
-    public int getElement(int y, int x) { //y is row x is column
-        if ((x >= 0 && x < matrix[0].length) && (y >= 0 && y < matrix.length)){
+    public int getElement(int y, int x) { // y is row x is column
+        if ((x >= 0 && x < matrix[0].length) && (y >= 0 && y < matrix.length)) {
             return matrix[y][x];
         }
-
-        //throw new java.lang.UnsupportedOperationException("TODO!");
+        throw new IllegalArgumentException();
     }
 
     @Override
     public int getRows() {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        return matrix.length;
     }
 
     @Override
     public int getColumns() {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        if(matrix.length == 0) //edgecase 0x0
+            return 0;
+        return matrix[0].length;
     }
 
     @Override
     public Matrix scale(int scalar) {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        int[][] newMatrix = new int[getRows()][getColumns()]; // new matrix using same size with get function
+        for (int y = 0; y < getRows(); y++) {
+            for (int x = 0; x < getColumns(); x++) {
+                newMatrix[y][x] = matrix[y][x] * scalar; // scaling function
+            }
+        }
+        return new CompletedMatrix(newMatrix);
     }
 
     @Override
     public Matrix plus(Matrix other) {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        int[][] newMatrix = new int[getRows()][getColumns()]; // new matrix using same size with get function
+        for (int y = 0; y < getRows(); y++) {
+            for (int x = 0; x < getColumns(); x++) {
+                newMatrix[y][x] = matrix[y][x] + other.getElement(y, x); // scaling function
+            }
+        }
+        return new CompletedMatrix(newMatrix);
     }
 
     @Override
     public Matrix minus(Matrix other) {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        int[][] newMatrix = new int[getRows()][getColumns()]; // new matrix using same size with get function
+        for (int y = 0; y < getRows(); y++) {
+            for (int x = 0; x < getColumns(); x++) {
+                newMatrix[y][x] = matrix[y][x] - other.getElement(y, x); // scaling function
+            }
+        }
+        return new CompletedMatrix(newMatrix);
     }
 
     @Override
     public Matrix multiply(Matrix other) {
-        throw new java.lang.UnsupportedOperationException("TODO!");
+        int[][] result = new int[getRows()][other.getColumns()]; // new matrix
+        for (int y = 0; y < getRows(); y++) { // set y to be row
+            for (int x = 0; x < other.getColumns(); x++) { // set x to be the given matrix to use for multiplying
+                int sum = 0;
+                for (int k = 0; k < getColumns(); k++) { // let k be the original matrix, column var
+                    sum += matrix[y][k] * other.getElement(k, x);
+                }
+                result[y][x] = sum;
+            }
+        }
+        return new CompletedMatrix(result);
     }
 
     /**
      * Entry point for matrix testing.
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        //These tests show sample usage of the matrix, and some basic ideas for testing. They are not comprehensive.
+        // These tests show sample usage of the matrix, and some basic ideas for
+        // testing. They are not comprehensive.
 
         int[][] data1 = new int[0][0];
-        int[][] data2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int[][] data3 = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
-        int[][] data4 = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
-        int[][] data5 = {{1, 4, 7}, {2, 5, 8}};
+        int[][] data2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] data3 = { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } };
+        int[][] data4 = { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } };
+        int[][] data5 = { { 1, 4, 7 }, { 2, 5, 8 } };
 
         Matrix m1 = new CompletedMatrix(data1);
         Matrix m2 = new CompletedMatrix(data2);
@@ -79,20 +114,20 @@ public class CompletedMatrix implements Matrix {
         System.out.println("m2 --> Rows: " + m2.getRows() + " Columns: " + m2.getColumns());
         System.out.println("m3 --> Rows: " + m3.getRows() + " Columns: " + m3.getColumns());
 
-        //check for reference issues
+        // check for reference issues
         System.out.println("m2 -->\n" + m2);
         data2[1][1] = 101;
         System.out.println("m2 -->\n" + m2);
 
-        //test equals
-        System.out.println("m2==null: " + m2.equals(null));             //false
-        System.out.println("m3==\"MATRIX\": " + m2.equals("MATRIX"));   //false
-        System.out.println("m2==m1: " + m2.equals(m1));                 //false
-        System.out.println("m2==m2: " + m2.equals(m2));                 //true
-        System.out.println("m2==m3: " + m2.equals(m3));                 //false
-        System.out.println("m3==m4: " + m3.equals(m4));                 //true
+        // test equals
+        System.out.println("m2==null: " + m2.equals(null)); // false
+        System.out.println("m3==\"MATRIX\": " + m2.equals("MATRIX")); // false
+        System.out.println("m2==m1: " + m2.equals(m1)); // false
+        System.out.println("m2==m2: " + m2.equals(m2)); // true
+        System.out.println("m2==m3: " + m2.equals(m3)); // false
+        System.out.println("m3==m4: " + m3.equals(m4)); // true
 
-        //test operations (valid)
+        // test operations (valid)
         System.out.println("m1 + m1:\n" + m1.plus(m1));
         System.out.println("m1 + m1:\n" + m1.plus(m1));
         System.out.println("2 * m2:\n" + m2.scale(2));
@@ -100,11 +135,11 @@ public class CompletedMatrix implements Matrix {
         System.out.println("m2 - m3:\n" + m2.minus(m3));
         System.out.println("3 * m5:\n" + m5.scale(3));
 
-        //not tested... multiply(). you know what to do.
+        // not tested... multiply(). you know what to do.
 
-        //test operations (invalid)
-        //System.out.println("m1 + m2" + m1.plus(m2));
-        //System.out.println("m1 + m5" + m1.plus(m5));
-        //System.out.println("m1 - m2" + m1.minus(m2));
+        // test operations (invalid)
+        // System.out.println("m1 + m2" + m1.plus(m2));
+        // System.out.println("m1 + m5" + m1.plus(m5));
+        // System.out.println("m1 - m2" + m1.minus(m2));
     }
 }

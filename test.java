324787ec6d20e0a5e84024b3a9dd07422c77aa20
Mitsubachi1 <60766345+Mitsubachi1@users.noreply.github.com>
2023-10-28 10:12:45 -0700

    Create test.java

---
+++ test.java
@@ -0,0 +1,29 @@
+public class test {
+    public static void main(String[] c) {
+        int n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 10; j < n; j++) {
+                System.out.print("");
+            }
+        }
+
+        for (int i = 1; i < n; i *= 3) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("");
+            }
+        }
+
+        int address = 0, step = 100;
+        while (address < n) {
+            while (step * 2 < n) {
+                System.out.print("");
+                step *= 2;
+            }
+            step = 100;
+            address += 10;
+        }
+
+    }
+
+}

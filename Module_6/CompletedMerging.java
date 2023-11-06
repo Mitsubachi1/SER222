04a52dde7940267a37bdf6e53a45db11f28db2de
Mitsubachi1 <60766345+Mitsubachi1@users.noreply.github.com>
2023-11-05 22:43:08 -0800

    Update CompletedMerging.java

--- Module_6/CompletedMerging.java
+++ Module_6/CompletedMerging.java
@@ -1,5 +1,6 @@
 //package edu.ser222.m02_02;
 package Module_6;
+
 /**
  * Implements various divide and conquer algorithms.
  *
@@ -14,12 +15,10 @@ import java.util.Random;
 
 public class CompletedMerging implements MergingAlgorithms {
 
-    //TODO: implement interface methods.
+    // TODO: implement interface methods.
     @Override
     public <T extends Comparable> Queue<T> mergeQueues(Queue<T> q1, Queue<T> q2) {
         Queue<T> mergedQueue = new ListQueue<>();
-
-
 
         // Add any remaining elements from q1 and q2, if any
         while (!q1.isEmpty()) {
@@ -32,7 +31,6 @@ public class CompletedMerging implements MergingAlgorithms {
 
         return mergedQueue;
     }
-    
 
     @Override
     public void sort(Comparable[] a) {
@@ -57,16 +55,15 @@ public class CompletedMerging implements MergingAlgorithms {
         left = mergesort(left);
         right = mergesort(right);
 
-
         return merge(left, right);
     }
 
     @Override
     public Comparable[] merge(Comparable[] a, Comparable[] b) {
         Comparable[] result = new Comparable[a.length + b.length];
-                                                                        //!issue here
+        // !issue here
         int i = 0, j = 0, k = 0;
-        
+
         while (i < a.length && j < b.length) {
             if (a[i].compareTo(b[j]) <= 0) {
                 result[k] = a[i];
@@ -96,13 +93,11 @@ public class CompletedMerging implements MergingAlgorithms {
     @Override
     public void shuffle(Object[] a) {
         Random rand = new Random();
-
-
         int n = a.length;
-        Object[] shuffled = new Object[n];
+        Object[] shuffled = new Object[n]; // new arr
 
         for (int i = 0; i < n; i++) {
-            int j = rand.nextInt(i + 1);
+            int j = rand.nextInt(i + 1); // using random
 
             if (j != i) {
                 shuffled[i] = shuffled[j];
@@ -110,66 +105,86 @@ public class CompletedMerging implements MergingAlgorithms {
 
             shuffled[j] = a[i];
         }
-
-        System.arraycopy(shuffled, 0, a, 0, n);
+        System.arraycopy(shuffled, 0, a, 0, n); // move over to a
     }
-    
-     
+
     /**
      * entry point for sample output.
      * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Queue<String> q1 = new ListQueue<>(); q1.enqueue("E"); q1.enqueue("L"); q1.enqueue("O"); q1.enqueue("R"); q1.enqueue("T");
-        Queue<String> q2 = new ListQueue<>(); q2.enqueue("A"); q2.enqueue("E"); q2.enqueue("M"); q2.enqueue("P"); q2.enqueue("S"); q2.enqueue("X");
-        Queue<Integer> q3 = new ListQueue<>(); q3.enqueue(5); q3.enqueue(12); q3.enqueue(15); q3.enqueue(17); q3.enqueue(20);
-        Queue<Integer> q4 = new ListQueue<>(); q4.enqueue(1); q4.enqueue(4); q4.enqueue(12); q4.enqueue(13); q4.enqueue(16); q4.enqueue(18);
+        Queue<String> q1 = new ListQueue<>();
+        q1.enqueue("E");
+        q1.enqueue("L");
+        q1.enqueue("O");
+        q1.enqueue("R");
+        q1.enqueue("T");
+        Queue<String> q2 = new ListQueue<>();
+        q2.enqueue("A");
+        q2.enqueue("E");
+        q2.enqueue("M");
+        q2.enqueue("P");
+        q2.enqueue("S");
+        q2.enqueue("X");
+        Queue<Integer> q3 = new ListQueue<>();
+        q3.enqueue(5);
+        q3.enqueue(12);
+        q3.enqueue(15);
+        q3.enqueue(17);
+        q3.enqueue(20);
+        Queue<Integer> q4 = new ListQueue<>();
+        q4.enqueue(1);
+        q4.enqueue(4);
+        q4.enqueue(12);
+        q4.enqueue(13);
+        q4.enqueue(16);
+        q4.enqueue(18);
 
         MergingAlgorithms ma = new CompletedMerging();
 
-        //Q1 - sample test cases
+        // Q1 - sample test cases
         Queue merged1 = ma.mergeQueues(q1, q2);
         System.out.println(merged1.toString());
         Queue merged2 = ma.mergeQueues(q3, q4);
         System.out.println(merged2.toString());
-        
-        //Q2 - sample test cases
-        String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+
+        // Q2 - sample test cases
+        String[] a = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
         ma.sort(a);
         assert isSorted(a);
         show(a);
-        
-        //Q3 - sample test cases
-        String[] b = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+
+        // Q3 - sample test cases
+        String[] b = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
         ma.shuffle(b);
         show(b);
-        
+
         ma.shuffle(b);
         show(b);
     }
 
-    //below are utilities functions, please do not change them.
-        
-    //sorting helper from text
+    // below are utilities functions, please do not change them.
+
+    // sorting helper from text
     private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
 
-    //sorting helper from text
+    // sorting helper from text
     private static void show(Comparable[] a) {
         for (Comparable a1 : a)
             System.out.print(a1 + " ");
 
         System.out.println();
     }
-    
-    //sorting helper from text
+
+    // sorting helper from text
     public static boolean isSorted(Comparable[] a) {
         for (int i = 1; i < a.length; i++)
-            if (less(a[i], a[i-1]))
+            if (less(a[i], a[i - 1]))
                 return false;
-        
+
         return true;
     }
 }
